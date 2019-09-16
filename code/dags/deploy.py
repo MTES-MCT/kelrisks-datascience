aa4d25323dfdae51a5046a446a8a17c43f37a9c3
benoitguigal <benoit.guigal@gmail.com>
2019-09-16 14:38:53 +0200

    create id sequences in deploy

--- code/dags/deploy.py
+++ code/dags/deploy.py
@@ -1,10 +1,12 @@
 # -*- coding=utf-8 -*-
 
+import textwrap
 from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.data_preparation import CopyTableOperator
+from airflow.operators.postgres_operator import PostgresOperator
 
 import helpers
 from config import CONN_ID
@@ -30,11 +32,27 @@ with DAG("deploy",
         source="etl.cadastre",
         destination="kelrisks.cadastre")
 
+    create_cadastre_id_seq = PostgresOperator(
+        task_id="create_cadastre_id_seq",
+        postgres_conn_id=CONN_ID,
+        sql=textwrap.dedent("""
+            DROP SEQUENCE IF EXISTS cadastre_id_seq;
+            CREATE SEQUENCE cadastre_id_seq;
+            ALTER SEQUENCE cadastre_id_seq owner to postgres;"""))
+
     deploy_sis = CopyTableOperator(
         task_id="deploy_sis",
         postgres_conn_id=CONN_ID,
         source="etl.sis",
         destination="kelrisks.sis")
+
+    create_sis_id_seq = PostgresOperator(
+        task_id="create_sis_id_seq",
+        postgres_conn_id=CONN_ID,
+        sql=textwrap.dedent("""
+            DROP SEQUENCE IF EXISTS sis_id_seq;
+            CREATE SEQUENCE sis_id_seq;
+            ALTER SEQUENCE sis_id_seq owner to postgres;"""))
 
     deploy_basol = CopyTableOperator(
         task_id="deploy_basol",
@@ -42,11 +60,27 @@ with DAG("deploy",
         source="etl.basol",
         destination="kelrisks.basol")
 
+    create_basol_id_seq = PostgresOperator(
+        task_id="create_basol_id_seq",
+        postgres_conn_id=CONN_ID,
+        sql=textwrap.dedent("""
+            DROP SEQUENCE IF EXISTS basol_id_seq;
+            CREATE SEQUENCE basol_id_seq;
+            ALTER SEQUENCE basol_id_seq owner to postgres;"""))
+
     deploy_basias = CopyTableOperator(
         task_id="deploy_basias",
         postgres_conn_id=CONN_ID,
         source="etl.basias",
         destination="kelrisks.basias")
+
+    create_basias_id_seq = PostgresOperator(
+        task_id="create_basias_id_seq",
+        postgres_conn_id=CONN_ID,
+        sql=textwrap.dedent("""
+            DROP SEQUENCE IF EXISTS basias_id_seq;
+            CREATE SEQUENCE basias_id_seq;
+            ALTER SEQUENCE basias_id_seq owner to postgres;"""))
 
     deploy_s3ic = CopyTableOperator(
         task_id="deploy_s3ic",
@@ -54,15 +88,45 @@ with DAG("deploy",
         source="etl.s3ic",
         destination="kelrisks.s3ic")
 
+    create_s3ic_id_seq = PostgresOperator(
+        task_id="create_s3ic_id_seq",
+        postgres_conn_id=CONN_ID,
+        sql=textwrap.dedent("""
+            DROP SEQUENCE IF EXISTS s3ic_id_seq;
+            CREATE SEQUENCE s3ic_id_seq;
+            ALTER SEQUENCE s3ic_id_seq owner to postgres;"""))
+
     deploy_code_postal = CopyTableOperator(
         task_id="deploy_code_postal",
         postgres_conn_id=CONN_ID,
         source="etl.code_postal",
         destination="kelrisks.adresse_commune")
 
+    create_code_postal_id_seq = PostgresOperator(
+        task_id="create_code_postal_id_seq",
+        postgres_conn_id=CONN_ID,
+        sql=textwrap.dedent("""
+            DROP SEQUENCE IF EXISTS adresse_commune_id_seq;
+            CREATE SEQUENCE adresse_commune_id_seq;
+            ALTER SEQUENCE adresse_commune_id_seq owner to postgres;"""))
+
     start >> [
         deploy_cadastre,
         deploy_sis,
         deploy_basol,
         deploy_basias,
-        deploy_s3ic]
+        deploy_s3ic,
+        deploy_code_postal]
+
+    deploy_cadastre >> create_cadastre_id_seq
+
+    deploy_sis >> create_sis_id_seq
+
+    deploy_basol >> create_basol_id_seq
+
+    deploy_basias >> create_basias_id_seq
+
+    deploy_s3ic >> create_s3ic_id_seq
+
+    deploy_code_postal >> create_code_postal_id_seq
+
